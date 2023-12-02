bc2db4eebda8d67d7928e551852c128104c6e972
jim-kns <looks.need-0d@icloud.com>
2023-12-02 11:35:53 +0630

    draft randomContacts.js

--- randomContacts.js
+++ randomContacts.js
@@ -1,6 +1,7 @@
-// goal [{key : 1, name : 'alice op', phone : '123-123-123'}, ...]
+// goal   [ { key: 1, name: 'Emanuel Sexton', phone: '708-833-506' },
+  // { key: 2, name: 'Joel Frank', phone: '433-352-239' }, ]
 
-const numContacts = 10;
+const numContacts = 20;
 
 const firstNames = [
   "Yaritza",
@@ -43,9 +44,6 @@ const firstNames = [
   "Davon",
   "Donna",
   "Natalya",
-];
-
-const lastNames = [
   "Adeline",
   "Dayami",
   "Mikayla",
@@ -67,6 +65,81 @@ const lastNames = [
   "Maribel",
 ];
 
-// const nameGenerator = ()
+const lastNames = [
+  "Pruitt",
+  "Moody",
+  "Oneill",
+  "Martinez",
+  "Chavez",
+  "Atkinson",
+  "Carroll",
+  "Bryan",
+  "Heath",
+  "Parker",
+  "Forbes",
+  "Dominguez",
+  "Booker",
+  "Hodge",
+  "Gould",
+  "Jones",
+  "Nielsen",
+  "Shaw",
+  "Meyer",
+  "Mcpherson",
+  "Pena",
+  "Trevino",
+  "Ellis",
+  "Sellers",
+  "Poole",
+  "Moyer",
+  "Quinn",
+  "Lambert",
+  "Gray",
+  "Lee",
+  "Welch",
+  "Sexton",
+  "Drake",
+  "Beard",
+  "Reyes",
+  "Mills",
+  "Bird",
+  "Frank",
+  "Livingston",
+  "Yates",
+ 
+];
 
-// console.log("hello");
+const getRandomInt = ( max, min = 0) => Math.floor(Math.random() * (max - min + 1) + min)
+
+
+const getRandomName = () => {
+  const firstNamesRange = firstNames.length - 1
+  const lastNamesRange = lastNames.length - 1
+  const randomFirstName = firstNames[getRandomInt(firstNamesRange)]
+  const randomLastName = lastNames[getRandomInt(lastNamesRange)]
+  return `${randomFirstName} ${randomLastName}`
+}
+
+const getRandomPhone = () => { 
+  const randInt1 = getRandomInt(999,100)
+  const randInt2 = getRandomInt(999,100)
+  const randInt3 = getRandomInt(999,100)
+  return `${randInt1}-${randInt2}-${randInt3}`
+
+  
+}
+
+
+const getRandomContact = () => ({ name : getRandomName(), phone : getRandomPhone()})
+
+const addKey = (contact,key) => ({ key, ...contact})
+
+// console.log(getRandomInt())
+// console.log(getRandomName());
+// console.log(getRandomPhone());
+// console.log(getRandomContact());
+
+const randomContacts = () => Array.from({length : numContacts}, getRandomContact).map(addKey)
+
+export {randomContacts};
+// console.log( randomContacts());
