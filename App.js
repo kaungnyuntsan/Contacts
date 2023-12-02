eee7d887176fb989bdf08199d40c29e8fa394270
jim-kns <looks.need-0d@icloud.com>
2023-12-02 18:28:30 +0630

    working

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { randomContacts } from './randomContacts';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {randomContacts().map(contact => (
+        <Text> {contact.name} </Text>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
