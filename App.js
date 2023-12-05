f573d3a5e6d655f4673dac0c6b4d1ee777b42ebd
Kaung-Nyunt-San <71218665+Kaung-Nyunt-San@users.noreply.github.com>
2023-12-05 22:56:52 +0630

    draft add contact

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import {
   Button,
   FlatList,
   SectionList,
+  TextInput,
 } from "react-native";
 import randomContacts, { objSortByName } from "./randomContacts";
 import { ContactRow } from "./ContactRow";
@@ -18,13 +19,42 @@ export default function App() {
   const [contactsView, setContactsView] = useState(true);
   const [contacts, setContacts] = useState(randomContacts);
 
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
   return (
     <SafeAreaView style={styles.container}>
-      <Button title="toggle" onPress={() => setContactsView(!contactsView)} />
+      {/* <Button title="toggle" onPress={() => setContactsView(!contactsView)} />
       <Button
         title="sort"
         onPress={() => {
           setContacts([...contacts].sort(objSortByName));
+        }}
+      /> */}
+      <TextInput
+        style={{ borderWidth: 1, margin: 10, padding: 10 }}
+        placeholder="name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={{ borderWidth: 1, margin: 10, padding: 10 }}
+        placeholder="phone"
+        value={phone}
+        keyboardType="numeric"
+        onChangeText={setPhone}
+      />
+      <Button
+        title="add"
+        onPress={() => {
+          setContacts([
+            ...contacts,
+            {
+              key: contacts.length + 1,
+              name,
+              phone,
+            },
+          ]);
         }}
       />
       {contactsView && <ContactsList contacts={contacts} />}
