9b8813f4e0f187d76d3699ae511719fae9b75ab5
Kaung-Nyunt-San <71218665+Kaung-Nyunt-San@users.noreply.github.com>
2023-12-07 12:18:17 +0630

    add form initial

---
+++ AddContactForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { TextInput, View, Button } from "react-native";
+
+const AddContactForm = ({ addContact }) => {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  return (
+    <View>
+      <TextInput
+        style={{ borderWidth: 1, margin: 10, padding: 10 }}
+        placeholder="name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={{ borderWidth: 1, margin: 10, padding: 10 }}
+        placeholder="phone"
+        value={phone}
+        keyboardType="numeric"
+        onChangeText={setPhone}
+      />
+      <Button
+        title="add"
+        onPress={() => {
+          addContact(name, phone);
+        }}
+      />
+    </View>
+  );
+};
+
+export { AddContactForm };
--- App.js
+++ App.js
@@ -14,24 +14,37 @@ import {
 import randomContacts, { objSortByName } from "./randomContacts";
 import { ContactRow } from "./ContactRow";
 import { ContactsList } from "./ContactsList";
+import { AddContactForm } from "./AddContactForm";
 
 export default function App() {
   const [contactsView, setContactsView] = useState(true);
   const [contacts, setContacts] = useState(randomContacts);
 
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
+  const addContact = (name, phone) => {
+    setContacts([
+      ...contacts,
+      {
+        key: contacts.length + 1,
+        name,
+        phone,
+      },
+    ]);
+    setContactsView(true);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      {/* <Button title="toggle" onPress={() => setContactsView(!contactsView)} />
       <Button
+        title="Add Contact"
+        onPress={() => setContactsView(!contactsView)}
+      />
+      {/* <Button
         title="sort"
         onPress={() => {
           setContacts([...contacts].sort(objSortByName));
         }}
       /> */}
-      <TextInput
+      {/* <TextInput
         style={{ borderWidth: 1, margin: 10, padding: 10 }}
         placeholder="name"
         value={name}
@@ -56,8 +69,12 @@ export default function App() {
             },
           ]);
         }}
-      />
-      {contactsView && <ContactsList contacts={contacts} />}
+      /> */}
+      {contactsView ? (
+        <ContactsList contacts={contacts} />
+      ) : (
+        <AddContactForm addContact={addContact} />
+      )}
     </SafeAreaView>
   );
 }
