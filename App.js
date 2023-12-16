f0af64294ad8174395c223c7981bb01f6c5bdc2c
Kaung-Nyunt-San <71218665+Kaung-Nyunt-San@users.noreply.github.com>
2023-12-16 22:24:17 +0630

    add icon

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { useState, useReducer, useContext } from "react";
-import { StyleSheet, Button, View, Text } from "react-native";
+import { StyleSheet, Button, View, Text, SafeAreaView } from "react-native";
 import randomContacts from "./randomContacts";
 import { ContactsList } from "./ContactsList";
 import { AddContactForm } from "./AddContactForm";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { contactsReducer } from "./contactsReducer";
 import { ContactsContext, ContactsDispatchContext } from "./ContactsContext";
+import { SettingsScreen } from "./SettingsScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 function HomeScreen({ navigation }) {
   const [contactsView, setContactsView] = useState(true);
@@ -57,31 +59,61 @@ function DetailsScreen({ route }) {
 }
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const ContactsScreen = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+        options={({ route }) => ({ title: route.params.name })}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
   const [contacts, dispatch] = useReducer(contactsReducer, randomContacts);
 
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <ContactsContext.Provider value={contacts}>
-          <ContactsDispatchContext.Provider value={dispatch}>
-            <Stack.Navigator>
-              <Stack.Screen
-                name="Home"
-                component={HomeScreen}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="Details"
-                component={DetailsScreen}
-                options={({ route }) => ({ title: route.params.name })}
-              />
-            </Stack.Navigator>
-          </ContactsDispatchContext.Provider>
-        </ContactsContext.Provider>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <ContactsContext.Provider value={contacts}>
+        <ContactsDispatchContext.Provider value={dispatch}>
+          <Tab.Navigator
+            // initialRouteName="Settings"
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                const icons = {
+                  Contacts: focused ? "people" : "people-outline",
+                  Settings: focused ? "settings" : "settings-outline",
+                };
+
+                return (
+                  <Ionicons
+                    name={icons[route.name]}
+                    size={size}
+                    color={color}
+                  />
+                );
+              },
+            })}
+          >
+            <Tab.Screen
+              name="Contacts"
+              component={ContactsScreen}
+              options={{ headerShown: false }}
+            />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </ContactsDispatchContext.Provider>
+      </ContactsContext.Provider>
+    </NavigationContainer>
   );
 }
 
